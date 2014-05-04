ff9ea4179320e5aab2bb91b5650016ad06463ceb
Kiyan <freetymesunkiyan@gmail.com>
2014-05-04 12:23:22 -0400

    add AES.py, ApplyAes.oy

---
+++ AES/AES.py
@@ -0,0 +1,15 @@
+# an implementation of AES
+
+# a sample run of AES
+def main():
+	return
+
+# takes string text as an input
+# output the encrypted text
+def aes(text):
+	msg = ''
+	
+	return msg
+
+if __name__ == '__main__':
+	main()
