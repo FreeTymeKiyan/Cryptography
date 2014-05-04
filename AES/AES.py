1860dc1788379acf4c4bf3516e0d3803329bfe36
Kiyan <freetymesunkiyan@gmail.com>
2014-05-04 13:28:07 -0400

    modify AES.py, basic architecture

--- AES/AES.py
+++ AES/AES.py
@@ -8,8 +8,26 @@ def main():
 # output the encrypted text
 def aes(text):
 	msg = ''
-	
+	addRoundKey()
+	for i in range(10):
+		byteSub()
+		shiftRow()
+		if i != 9:
+			mixColumn()
+		addRoundKey()				
 	return msg
+
+def byteSub():
+	return 
+
+def shiftRow():
+	return
+
+def mixColumn():
+	return
+
+def addRoundKey():
+	return
 
 if __name__ == '__main__':
 	main()
